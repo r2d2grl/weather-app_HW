841c192fd03f8e1113e0a44b51485226847d57f2
r2d2grl <r2d2krl@gmail.com>
2024-05-26 22:53:22 +0300

    Update

--- src/index.js
+++ src/index.js
@@ -17,6 +17,8 @@ function refreshWeather(response) {
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
   iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon"/>`;
+
+  getForecast(response.data.city);
 }
 
 function formatDate(date) {
@@ -60,5 +62,81 @@ function handleSearchSubmit(event) {
   // call the API
   // search for the city
 }
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
+function getForecast(city) {
+  let apiKey = "03bee20de1a30b2029956ae8f8td490o";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+  axios(apiUrl).then(displayForecast);
+  console.log(apiUrl);
+}
+
+function displayForecast(response) {
+  // console.log(response.data);
+
+  let forecastHtml = "";
+
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
+      <div class="weather-forecast">
+          <div class="col-2">
+              <div class="weather-forecast-date">${formatDay(day.time)}</div>
+              <img src="${
+                day.condition.icon_url
+              }" class="weather-forecast-icon"/>
+              <div class="weather-forecast-temperatures">
+                <div class="weather-forecast-temperature-max"> ${Math.round(
+                  day.temperature.maximum
+                )}°</div>
+                <div class="weather-forecast-temperature-min">${Math.round(
+                  day.temperature.minimum
+                )}°</div>
+              </div>
+          </div>      
+      </div>
+      `;
+    }
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
+function getSeason() {
+  const month = new Date().getMonth() + 1;
+  if (month >= 3 && month <= 5) {
+    document.body.background = "";
+    // закомментировал фоновые рисунки
+    //      "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/115/098/original/Spring-W-46.jpg";
+    return `spring`;
+  } else if (month >= 6 && month <= 8) {
+    document.body.background = "";
+    //      "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/115/099/original/Summer-W-46.jpg";
+    return `summer`;
+  } else if (month >= 9 && month <= 11) {
+    document.body.background = "";
+    //      "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/115/100/original/Autumn-W-46.jpg";
+    return `autumn`;
+  } else {
+    document.body.background = "";
+    //      "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/114/931/original/Winter-W.jpg";
+    return `winter`;
+  }
+}
+
+let Season = getSeason();
+console.log(Season);
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Kharkiv");
